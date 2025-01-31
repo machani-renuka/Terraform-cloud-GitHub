a033c90d99eef13dbdd8e2c528a25c6b6e42d969
machani-renuka <machanirenuka9014@gmail.com>
2025-01-31 19:37:51 +0530

    Update main.tf renuka changed infra

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Renukainfra" {
+resource "aws_instance" "Renuinfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.micro"
 }
